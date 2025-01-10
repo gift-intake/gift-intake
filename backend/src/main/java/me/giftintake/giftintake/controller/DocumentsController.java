d0fe45b26135adc69640fe0b5e6e99a8180ba2c8
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-01-10 10:23:45 -0600

    Update DocumentsController.java

--- backend/src/main/java/me/giftintake/giftintake/controller/DocumentsController.java
+++ backend/src/main/java/me/giftintake/giftintake/controller/DocumentsController.java
@@ -4,8 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 /**
- * Handles the requests for
+ * Handles the requests for uploading documents to be processed.
  */
 @RestController
 @RequestMapping("/api/documents")
@@ -21,4 +23,23 @@ public final class DocumentsController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Gets the status of a document
+     * @param id the id of the document
+     * @return a response entity
+     */
+    @GetMapping("/status")
+    public ResponseEntity<Void> documentStatus(@RequestParam("id") UUID id) {
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Gets the result of a document
+     * @param id the id of the document
+     * @return a response entity
+     */
+    @GetMapping("/result")
+    public ResponseEntity<Void> documentResult(@RequestParam("id") UUID id) {
+        return ResponseEntity.ok().build();
+    }
 }
