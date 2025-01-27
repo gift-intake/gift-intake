d874f77cadbc975fb69f7864851e1aa9edfd2fb9
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-01-26 23:32:44 -0600

    write basic code to prompt the models on labels

--- machine-learning/machine_learning/__init__.py
+++ machine-learning/machine_learning/__init__.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from gliner import GLiNER
+from typing import List
 
+model = GLiNER.from_pretrained("urchade/gliner_large-v2.1")
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    pass
+class Email(BaseModel):
+    text: str
+
+class EmailsList(BaseModel):
+    emails: List[Email]
+
+@app.post("/inference")
+async def inference(input_data: EmailsList):
+    results = []
+
+    for email in input_data.emails:
+        entities = model.predict_entities(email.text, ["Interval","Frequency","Date","Money","GiftType", "Department", "Person"], threshold=0.8)
+        results.append({
+            "text": email.text,
+            "entities": entities
+        })
+
+    return {
+        "results": results
+    }
