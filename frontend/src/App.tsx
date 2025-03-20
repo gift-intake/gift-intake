224412450232f47c02b706a43386cc59e7e7e7f7
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-03-19 22:17:09 -0500

    create init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-1/2 text-center">
+        <h1 className="text-2xl font-semibold">Upload your documents?</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
