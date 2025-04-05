38bc77930154be4d65e0b5f8ab05085c53c513ff
Maisha Nizami <102842955+maishanizami@users.noreply.github.com>
2025-04-05 12:35:12 -0500

    last updated code for .msg extraction

--- machine-learning/machine_learning/gliner_model.py
+++ machine-learning/machine_learning/gliner_model.py
@@ -28,34 +28,9 @@ labels = [
             "DonorAddress", "DonorCity", "DonorProvince", "DonorCountry", "DonorPhone",
             "DonorEmail", "GiftCurrency", "GiftDate", "PaymentMethod"]
 
-test_set_path = Path("results.jsonl")
-
 results = []
-
-with open(test_set_path, "r") as f:
-    for line in f:
-        raw_data = json.loads(line)
-        try:
-            result = Result(**raw_data)
-            filtered_entities = [entity for entity in result.entities if entity.types in labels]
-            result.entities = filtered_entities
-            results.append(result)
-        except Exception as e:
-            print(f"Error parsing line: {e}")
-            print(f"Problematic data: {raw_data}")
-
 model = GLiNER.from_pretrained("urchade/gliner_medium-v2.1")
+for label in labels:
+    results.append({"label": label, "value": []})
+    
 
-ner_results = []
-for result in results:
-    text = result.text
-    entities = model.predict_entities(text, labels)
-    classified = [Entity(entity=entity["text"], types=entity["label"]) for entity in entities]
-    ner_results.append(Result(text=text, entities=classified))
-
-# Save JSON Schema
-schema = generate_json_schema()
-with open("schema.json", "w") as f:
-    json.dump(schema, f, indent=4)
-
-print("JSON Schema saved to schema.json")
